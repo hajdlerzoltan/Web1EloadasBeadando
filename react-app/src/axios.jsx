4f955ea28c664a3f90c8596f7c00d4e946423b42
hajdlerzoltan <hajdlerzoltan@gmail.com>
2026-06-17 00:24:21 +0200

    last fix

--- react-app/src/axios.jsx
+++ react-app/src/axios.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = "/Web1Eloadas/php/api.php";
+const API = "/php/api.php";
 
 export default function Crud() {
 
